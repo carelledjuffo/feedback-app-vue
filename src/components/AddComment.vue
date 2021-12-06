5ce9cda411d302644fad510143b7d8ef6c160100
Carelle Djuffo <Carelle.Djuffo@ttr-group.de>
2021-12-06 09:44:11 +0100

    add all files

---
+++ src/components/AddComment.vue
@@ -0,0 +1,58 @@
+<template>
+  <form class="form-form">
+    <div class="form-group">
+      <label for="commentTitle">Title</label>
+      <input type="text" class="form-control" id="commentTitle" v-model="title">
+    </div>
+    <div class="form-group">
+      <label for="commentCategory">Category</label>
+      <input type="text" class="form-control" id="commentCategory" v-model="category">
+    </div>
+    <div class="form-group">
+      <label for="CommentDescription">Description</label>
+      <textarea class="form-control" id="CommentDescription" rows="3" v-model="description"></textarea>
+    </div>
+    <div class="form-group btn-action">
+      <button class="btn btn-secondary" @click="saveComment($event)">Submit</button>
+      <button class="btn btn-danger delete-btn">Cancel</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddComment",
+  data() {
+    return {
+      title: '',
+      category:'',
+      description:''
+    }
+  },
+  methods: {
+    saveComment(event) {
+      event.preventDefault();
+      let comment = {
+        title: this.title,
+        category: this.category,
+        description: this.description,
+        upvote: 0
+      }
+      this.$emit('newComment',comment);
+    },
+  }
+}
+</script>
+
+<style scoped>
+.form-group {
+  margin-top: 2em;
+}
+.btn-action {
+  display: flex;
+  justify-content: center;
+}
+.delete-btn {
+  margin-left: 1em;
+}
+</style>
---
+++ src/components/FeedBackMain.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="main-content">
+    <AddComment v-on:newComment="updateCommentList($event)" class="add-comment"></AddComment>
+    <div class="comment-main-display">
+      <div class="comment-header">
+        <p class="comment-amount">{{commentList.length}} Suggestions</p>
+        <button class="btn btn-primary add-btn">Add comment</button>
+      </div>
+      <div v-for="(comment, index) in commentList" v-bind:key="index" class="single-comment-display">
+        <CommentDisplay :comment="comment" ></CommentDisplay>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AddComment from "./AddComment.vue";
+import CommentDisplay from "./CommentDisplay";
+
+export default {
+  name: "App",
+  components: {
+    AddComment,
+    CommentDisplay,
+  },
+  data() {
+    return {
+      commentList: [
+        {
+          title: 'Question to the teacher',
+          category: 'Teacher',
+          description: 'Why are all teachers in this school alway very severe with students?',
+          upvote: 6
+        },
+        {
+          title: 'Question students',
+          category: 'Student',
+          description: 'I don\'t unterstand the last lesson on history of computer science',
+          upvote: 10
+        }
+      ],
+    };
+  },
+  methods: {
+    updateCommentList(comment) {
+      this.commentList.push(comment);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+
+}
+.comment-header {
+  height: 80px;
+  min-width: 100%;
+  background: #373F68;
+  color: white;
+  border-radius: 0.5em;
+  display: flex;
+  flex-flow: row;
+  justify-content: space-around;
+}
+.comment-main-display {
+  display: flex;
+  flex-flow: column;
+}
+.main-content {
+  display: flex;
+  justify-content: center;
+  flex-flow: row;
+  margin: 2em 2em 2em 2em;
+}
+.comment-main-display {
+  flex: 0 0 64%;
+}
+.add-btn {
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+.comment-amount {
+  font-weight:630 ;
+}
+.add-comment {
+  flex: 0 0 30%;
+  margin-right: 6%;
+}
+.comment-header > p {
+  align-self: center;
+  font-size: 1em;
+}
+
+@media only screen and (min-width: 1000px) {
+  .main-content{
+    margin: 2em 30em 2em 30em;
+  }
+}
+</style>
